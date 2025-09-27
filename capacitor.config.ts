78ebc5eba6e09449d73bba2bc11bf661c4305f31
joloLG <jlgracilla@sorsu.edu.ph>
2025-09-27 17:53:16 +0800

    Capacitor Added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.incidentSystem.mdrrmo',
+  appName: 'MDRRMO App Bulan',
+  webDir: 'public'
+};
+
+export default config;
